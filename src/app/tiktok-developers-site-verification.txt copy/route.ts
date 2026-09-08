3992b77ff57c9cf13514ab0f9bbe235da7486472
Azaz Asif <naturalsuper718@gmail.com>
2026-09-08 17:17:19 +0500

    new route for verfication set

---
+++ src/app/tiktok-developers-site-verification.txt copy/route.ts
@@ -0,0 +1,11 @@
+export async function GET() {
+  return new Response(
+    "tiktok-developers-site-verification=B64J8hGEM3pnox8tao983wWFgvB0NNN5",
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    },
+  );
+}
--- src/app/tiktok-developers-site-verification.txt/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET() {
-  return new Response("tiktok-developers-site-verification=lOUb2XaZTR1Fy3ZtwooJuoqDL3svpvFR", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain",
-    },
-  });
-}
